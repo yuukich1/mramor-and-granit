80a956add729c2ac5c425c688a2ed079c6181ae7
Konaisya <redgumaki@gmail.com>
2026-02-05 21:39:06 +0300

    feat: integrate Yandex Metrika for enhanced analytics tracking

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -74,6 +75,35 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106678263', 'ym');
+
+            ym(106678263, 'init', {
+                ssr: true,
+                webvisor: true, 
+                clickmap: true, 
+                ecommerce: "dataLayer", 
+                accurateTrackBounce: true, 
+                trackLinks: true
+            });
+          `}
+        </Script>
+
+        <noscript>
+          <div>
+            <img 
+              src="https://mc.yandex.ru/watch/106678263" 
+              style={{ position: 'absolute', left: '-9999px' }} 
+              alt="" 
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
